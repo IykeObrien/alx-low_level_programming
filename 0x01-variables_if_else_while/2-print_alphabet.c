abae9bd281c3398bd165c50d9a25c1d924416bb2
IykeObrien <ikechukwuogudu.io@gmail.com>
2022-09-09 14:23:09 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Prints numbers between 00 to 99.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints numbers between 00 to 99.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i, e, g, h, op1, op2;
-
 i = e = g = h = 48;
 while (h < 58)
 {
