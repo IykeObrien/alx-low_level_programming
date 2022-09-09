f246ffa5a922e0f4fde863e621c0e63797b1487c
IykeObrien <ikechukwuogudu.io@gmail.com>
2022-09-09 09:15:32 -0700

    task 7

--- 0x00-hello_world/betty.c
+++ 0x00-hello_world/betty.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-** main - Entry point
-**
-** Return: Always 0 (Success)
-**/
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
-:wq
 }
