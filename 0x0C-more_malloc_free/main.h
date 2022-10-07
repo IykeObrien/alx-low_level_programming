de1f82f525f1e79dff29d7cd11000e30c293602d
IykeObrien <ikechukwuogudu.io@gmail.com>
2022-10-07 14:13:38 -0700

    task 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * File: main
+ *
+ * Desc: header file
+ */
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
